67763b20fada1b501351c478a821a8675c57ee9f
n0wak <cudalb.vasile@gmail.com>
2024-08-19 00:52:15 +0300

    chore: check if dotnet sdk is already installed

--- src/DotNet/DotNet.ps1
+++ src/DotNet/DotNet.ps1
@@ -1,6 +1,21 @@
-﻿choco install -y dotnet-sdk --version=6.0.100;
-choco install -y dotnet-sdk --version=7.0.100;
-choco install -y dotnet-sdk --version=8.0.100;
+﻿$dotnetVersions = @("6.0.100", "7.0.100", "8.0.100")
+
+function Test-DotNetVersion {
+    param (
+        [string]$Version
+    )
+    $installedVersions = dotnet --list-sdks | ForEach-Object { $_.Split(' ')[0] }
+    return $installedVersions -contains $Version
+}
+
+foreach ($version in $dotnetVersions) {
+    if (Test-DotNetVersion -Version $version) {
+        Write-Host ".NET SDK $version is already installed."
+    } else {
+        Write-Host "Installing .NET SDK $version..."
+        choco install -y dotnet-sdk --version=$version
+    }
+}
 
 refreshenv;
 
