28bb9269aa03b1fb4c240ca73c6f262e2f018276
n0wak <cudalb.vasile@gmail.com>
2024-08-28 00:52:26 +0300

    feat: handle notepad++ and spotify installation

---
+++ src/Notepad++/Notepad++.ps1
@@ -0,0 +1,27 @@
+﻿Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath "..\Modules\Helpers\AppCheckUtilities.psm1")
+
+function Set-NotepadPlusPlus-Configuration {
+    $NotepadPlusPlusConfigurationPath = Join-Path -Path $env:appdata -ChildPath "Notepad++";
+    $DotfilesNotepadPlusPlusConfigurationPath = $PSScriptRoot;
+
+    if (-not (Test-Path -Path $NotepadPlusPlusConfigurationPath)) {
+        Write-Host "Notepad++ is not installed or configured." -ForegroundColor Yellow;
+    }
+    else {
+        Write-Host "Configuring Notepad++:" -ForegroundColor Cyan;
+
+        Get-ChildItem -Path "${DotfilesNotepadPlusPlusConfigurationPath}\*" -Include *.xml -Recurse | Copy-Item -Destination $NotepadPlusPlusConfigurationPath -Force;
+
+        Write-Host "Notepad++ was successfully configured." -ForegroundColor Green;
+    }
+}
+
+function Install {
+    if (-not (Confirm-AppInstalled -AppName "NotePad++")) {
+        choco install -y "notepadplusplus";
+    }
+    
+    Set-NotepadPlusPlus-Configuration;
+}
+
+Export-ModuleMember -Function Install
---
+++ src/Spotify/Spotify.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath "..\Modules\Helpers\AppCheckUtilities.psm1")
+
+function Install {
+    if (-not (Confirm-AppInstalled -AppName "Spotify")) {
+        choco install -y spotify
+    }
+}
+
+Export-ModuleMember -Function Install
