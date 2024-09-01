4c889712c57918904184132e758e5c9e23ccc680
n0wak <cudalb.vasile@gmail.com>
2024-09-01 23:34:02 +0300

    feat: install postman

---
+++ src/Postman/Postman.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath "..\Modules\Helpers\AppCheckUtilities.psm1")
+
+function Install {
+    if (-not (Confirm-AppInstalled -AppName "Postman")) {
+        choco install -y postman
+    }
+}
+
+Export-ModuleMember -Function Install
