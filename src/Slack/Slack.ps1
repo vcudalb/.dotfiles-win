9e5e988a1359a91e67e03f2f819f9ff006589015
n0wak <cudalb.vasile@gmail.com>
2024-09-02 00:07:33 +0300

    feat: slack installation

---
+++ src/Slack/Slack.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath "..\Modules\Helpers\AppCheckUtilities.psm1")
+
+function Install {
+    if (-not (Confirm-AppInstalled -AppName "Slack")) {
+        choco install -y slack
+    }
+}
+
+Export-ModuleMember -Function Install
